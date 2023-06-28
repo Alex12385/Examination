517caa9b2298c6c2fba2d3be5ddd004d88cdc113
Alex123 <you@example.comsamokhin_ay@techmoney.ru>
2023-06-28 17:25:30 +0300

    correct strucuture

--- Task1 Python Note/function.py
+++ Task1 Python Note/function.py
@@ -37,7 +37,7 @@ def showNote(text):
         print('Нет ни одной заметки...')
 
 
-def editNote(text):
+def refactorNote(text):
     id = input('Введите id необходимой заметки: ')
     array = fileWorker.readFile()
     logic = True
